c7ea1adfada42900dfdc9cfac5b3399f7b200be1
wealthchild <ebubechipeace@gmail.com>
2023-01-27 13:54:35 -0800

    task

--- 0x0C-more_malloc_free/2-add_node.c
+++ 0x0C-more_malloc_free/2-add_node.c
@@ -1,39 +1,24 @@
 #include "lists.h"
-#include <strings.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 /**
- * *add_node_end - adds a node at the end of a list
- * *@head:pointer to head
- * *@str:string to duplicate
- * *Return:new element
+ * *add_node - adds a new node at the beginning of a list_t list
+ * *@head:pointer to the first node
+ * *@str:string to add to new node
+ * *Return:address of new element,NULL otherwise
 */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 int len;
-list_t *last;
-list_t *new_node;
-last = *head;
-new_node = malloc(sizeof(list_t));
+list_t *new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 return (NULL);
 for (len = 0; str[len]; len++)
 ;
-new_node->str = strdup(str);
 new_node->len = len;
-new_node->next = NULL;
-if ((*head) == NULL)
-{
+new_node->str = strdup(str);
+new_node->next = (*head);
 (*head) = new_node;
-}
-else
-{
-last = *head;
-while (last->next != NULL)
-{
-last = last->next;
-}
-last->next = new_node;
-}
 return (new_node);
 }
